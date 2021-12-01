bc00309cbe53e58ebf608fcc54441be76942a1e6
malatini <malatini@student.42.fr>
2021-12-01 21:44:42 +0100

    mise en place commandes mahaut ok

--- includes/Commands.hpp
+++ includes/Commands.hpp
@@ -66,7 +66,7 @@ public:
 	** Le param de who pour correspondre au une channel ou a autre chose
 	** TODO: mahaut WHO
 	*/
-	void		who(Commands *command, Client *client, Server *server);
+	void		who(Client *client, Server *server);
 	static void	displayAllClients(Channel *channel);
 	//Appelera la sous fonction displayClientsFromChannel ?
 	static void	displayChannel(Channel *channel, Client *client);
@@ -81,7 +81,7 @@ public:
 	** TODO: Mahaut
 	** Voir comment le serveur doit reagir si il y a une nick collision
 	*/
-	void		kill(Commands *command, Client *client, Server *server, bool nick_collision);
+	void		kill(Client *client, Server *server, bool nick_collision);
 	//verifier les fonctions annexes relatives aux clients sont bien implementees
 	//nickname exists
 	//leave all channels
@@ -100,7 +100,7 @@ public:
 	 ** JOIN, MODE, KICK, PART, QUIT and PRIVMSG/NOTICE.
 	 ** See details on RFC 2812.
 	 */
-	void		join(Commands *command, Client *client, Server *server);
+	void		join(Client *client, Server *server);
 
 	/**
 	 * @brief 
@@ -110,7 +110,7 @@ public:
 	 * @param server 
 	 * TODO: par sure si cette commande est necessaire
 	 */
-	void		cap(Commands *command, Client *client, Server *server);
+	void		cap(Client *client, Server *server);
 
 	/**
 	 * @brief 
@@ -119,7 +119,7 @@ public:
 	 * @param client 
 	 * @param server 
 	 */
-	void		nick(Commands *command, Client *client, Server *server);
+	void		nick(Client *client, Server *server);
 	/*
 	** Autres fonctions necessaires a nick
 	*/
@@ -131,10 +131,81 @@ public:
 	 * @param command 
 	 * @param client 
 	 * @param server 
+	* The PART command causes the user sending the message to be removed
+	* from the list of active members for all given channels listed in the
+	* parameter string.  If a "Part Message" is given, this will be sent
+	* instead of the default message, the nickname.  This request is always
+	* granted by the server.
+	* Parameters: <channel> *( "," <channel> ) [ <Part Message> ]
 	 */
-	void		part(Commands *command, Client *client, Server *server);
+	void		part(Client *client, Server *server);
 	//verifier qu on a bien un leave channel dans client
 
+	/**
+	 * @brief 
+	 * 
+	 * @param client 
+	 * @param server 
+	 * The KICK command can be used to request the forced removal of a user
+   	 * from a channel.  It causes the <user> to PART from the <channel> by
+     * force.  For the message to be syntactically correct, there MUST be
+	 * either one channel parameter and multiple user parameter, or as many
+     * channel parameters as there are user parameters.  If a "comment" is
+     * given, this will be sent instead of the default message, the nickname
+     * of the user issuing the KICK.
+	 * Parameters: <channel> *( "," <channel> ) <user> *( "," <user> )
+               [<comment>]
+	 */
+	void		kick(Client *client, Server *server);
+
+	/**
+	 * @brief 
+	 * 
+	 * @param client 
+	 * @param server 
+	 * Parameters: <channel> [ <topic> ]
+	 * The TOPIC command is used to change or view the topic of a channel.
+     * The topic for channel <channel> is returned if there is no <topic>
+     * given.  If the <topic> parameter is present, the topic for that
+     * channel will be changed, if this action is allowed for the user
+     * requesting it.  If the <topic> parameter is an empty string, the
+     * topic for that channel will be removed.
+	 */
+	void		topic(Client *client, Server *server);
+
+	/**
+	 * @brief 
+	 * 
+	 * @param client 
+	 * @param server 
+	 * 	 * The list command is used to list channels and their topics.  If the
+     * <channel> parameter is used, only the status of that channel is
+     * displayed.
+
+   	 * If the <target> parameter is specified, the request is forwarded to
+     * that server which will generate the reply.
+	 * Parameters: [ <channel> *( "," <channel> ) [ <target> ] ]
+	 */
+	void		list(Client *client, Server *server);
+
+	/**
+	 * @brief 
+	 * 
+	 * @param client 
+	 * @param server 
+	 * By using the NAMES command, a user can list all nicknames that are
+     * visible to him. For more details on what is visible and what is not,
+     * see "Internet Relay Chat: Channel Management" [IRC-CHAN].  The
+     * <channel> parameter specifies which channel(s) to return information
+     * about.  There is no error reply for bad channel names.
+
+     * If no <channel> parameter is given, a list of all channels and their
+     * occupants is returned.  At the end of this list, a list of users who
+     * are visible but either not on any channel or not on a visible channel
+     * are listed as being on `channel' "*".
+	 * Parameters: [ <channel> *( "," <channel> ) [ <target> ] ]
+	 */
+	void		names(Client *client, Server *server);
 };
 
 /* 
