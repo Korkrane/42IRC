26c4a8fe4a5d80b9e3d8dbcc64a63f8b9234cb4d
malatini <malatini@student.42.fr>
2021-12-03 11:09:29 +0100

    save

---
+++ srcs/DisplayUtil/DisplayClients.cpp
@@ -0,0 +1,6 @@
+#include "../../includes/irc.hpp"
+
+/*
+** Idee : faire une fonction qui pourrait etre appelee n importe quand depuis le serveur
+** pour pouvoir verifier le nombre de clients qu il recense, voir meme si certains ont ete deconnecte ou pas
+*/
---
+++ srcs/DisplayUtil/DisplayParsing.cpp
@@ -0,0 +1,6 @@
+#include "../../includes/irc.hpp"
+
+/*
+** Proposition faire des fonctions qui seront utiles pour le dev / debug pour pouvoir afficher 
+** par exemple la commande parsing avant son execution
+*/
