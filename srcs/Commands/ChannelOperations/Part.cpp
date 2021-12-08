482b0e203686a658d62926e4fa6022c0bc58f589
malatini42 <malatini@student.42.fr>
2021-12-08 15:12:58 +0100

    debut part

--- srcs/Commands/ChannelOperations/Part.cpp
+++ srcs/Commands/ChannelOperations/Part.cpp
@@ -16,11 +16,34 @@ void  Commands::part(User *user, IRC *server)
 {
   (void)user;
   (void)server;
+  //Preparation du serveur d'arguments en cas d'ereur
+  std::vector<std::string> error;
   // Verifier le nombre d'argument, si il est egal a 1, renvoyer une erreur
+  if (user->get_params_size() == 1)
+  {
+      error.push_back(user->get_command_name())
+      error_handler("461", user, this, error);
+  }
+  //On sauvegarde les arguments qui suivent
+  std::string channel = params.front()
 
-  //On parse le deuxieme argument si ce n est pas deja fait ?
+  //On va sauvegarer tous les arguments qui suivent comme le message qui peut etre en differentes strings
+  std::vector<std::string>::iterator it = params.begin()++;
+  std::vector<std::string>::iterator ite  = params.end();
+  std::vector<std::string> message;
 
-  //On verifie que les autres arguments sont corrects (channel existants)
+  //TODO: faire une fonction separee pour pouvoir la reutiliser et tester
+  while (it != ite)
+  {
+    message.push_back(*it);
+    i++;
+  }
+  //On verifie que la channel existe bien le nom doit etre correct et la channel doit exister
+  if (is_correct_channel_name == false)
+  {
+      //NOSUCHCHANNEL
+      error.push_back();
+  }
 
   //Ensuite on va boucler sur les channels et voir si le client en membre
   //si il n est pas membre on retourne une erreur
