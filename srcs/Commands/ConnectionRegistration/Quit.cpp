d3cea891a02263dae95848b3fdc9f715d1724c80
Korkrane <baudoin.haas@gmail.com>
2021-12-10 15:07:18 +0100

    FIX: quit compil error

--- srcs/Commands/ConnectionRegistration/Quit.cpp
+++ srcs/Commands/ConnectionRegistration/Quit.cpp
@@ -1,39 +1,39 @@
 #include <IRC.hpp>
 
-void        Commands::send_quit_message(User *user, IRC *server, std::string message)
+void Commands::send_quit_message(User *user, IRC *server, std::string message)
 {
     (void)user;
     (void)server;
-    (void)params;
-    return ;
+    (void)message;
+    return;
 }
 
 //TODO: Mahaut + Baudoin (+Damien )?
-void        Commands::quit_cmd(User *user, IRC *server)
+void Commands::quit_cmd(User *user, IRC *server)
 {
     (void)user;
     (void)server;
 
     std::vector<std::string> params = user->get_params();
     int size = user->get_params_size();
-    std::string message;//Partie de la reponse qui sera envoyee au serveur
+    std::string message; //Partie de la reponse qui sera envoyee au serveur
     std::vector<std::string>::iterator it = params.begin();
     std::vector<std::string>::iterator ite = params.end();
 
     if (size == 1)
         message = "";
-    else 
+    else
     {
         //On va faire le tour des tous les params et reconstiuer le message
         while (it != ite)
         {
             message += (*it);
-            if (ite + 1 == ite)//pour eviter un espace vide a la fin
+            if (ite + 1 == ite) //pour eviter un espace vide a la fin
                 break;
             message += " ";
             it++;
         }
     }
-    send_quit_message(user, server, message);    
-    return ;
+    send_quit_message(user, server, message);
+    return;
 }
