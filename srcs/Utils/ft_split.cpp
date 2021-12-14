c8c9a46b8f807bac49a4986dd02a9109263bb5a2
malatini42 <malatini@student.42.fr>
2021-12-14 23:31:29 +0100

    split a changer

--- srcs/Utils/ft_split.cpp
+++ srcs/Utils/ft_split.cpp
@@ -9,7 +9,7 @@ void    User::ft_split_channels(std::string line, char charset)
 {
     std::string              buffer;
     size_t                   i = 0;
-    size_t                   pos = 0;
+    //size_t                 pos = 0;
     size_t                   len = line.length();
 
     if (line.empty())
@@ -21,22 +21,17 @@ void    User::ft_split_channels(std::string line, char charset)
     }
         
     //On va lire la string a splitter
-    while (pos < len)
+    while (i < len)
     {
-        pos = i;
         i = line.find(charset, pos);
         //Si on trouve quelque chose (\0) inclu
-        if (pos != i)
+        if (i == std::string::npos)
         {
-            //SI on est arrive au \0
-            if (i == std::string::npos)
-            {
-                std::cout << "line is " << line << std::endl;
-                this->_splitted_channels.push_back(line);
-                return ;
-            }
-            //TODO: a revoir (cas ou on trouve au moins une fois le char recherche)
+            //std::cout << "line is " << line << std::endl;
+            this->_splitted_channels.push_back(line);
+            return ;
         }
+        //TODO: a revoir (cas ou on trouve au moins une fois le char recherche)
     }
     return ;
 }
@@ -45,28 +40,24 @@ void    User::ft_split_args(std::string line, char charset)
 {
     std::string              buffer;
     size_t                   i = 0;
-    size_t                   pos = 0;
+    //size_t                 pos = 0;
     size_t                   len = line.length();
 
     if (line.empty())
         return ;
     //On va lire la string a splitter
-    while (pos < len)
+    while (i < len)
     {
-        pos = i;
+        //pos = i;
         i = line.find(charset, pos);
         //Si on trouve quelque chose (\0) inclu
-        if (pos != i)
+        if (i == std::string::npos)
         {
-            //SI on est arrive au \0
-            if (i == std::string::npos)
-            {
-                std::cout << "line is " << line << std::endl;
-                this->_splitted_args.push_back(line);
-                return ;
-            }
-            //TODO: a revoir (cas ou on trouve au moins une fois le char recherche)
+            //std::cout << "line is " << line << std::endl;
+            this->_splitted_args.push_back(line);
+            return ;
         }
+        //TODO: a revoir (cas ou on trouve au moins une fois le char recherche)
     }
     return ;
 }
@@ -105,9 +96,6 @@ std::vector<std::string>    ft_split(std::string line, std::string charset)
             //SI on est arrive au \0
             if (i == std::string::npos)
             {
-                #if DEBUG
-                    std::cout << BLUE << "DEBUG: " << "0. Splitting adding " << line.substr(pos) << std::endl;
-                #endif
                 splitted.push_back(line.substr(pos));
                 return (splitted);
             }
