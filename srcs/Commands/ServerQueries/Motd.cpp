c49be05468dcdd81da2f66e1f2cbdac476044685
malatini42 <malatini@student.42.fr>
2021-12-16 20:58:17 +0100

    save

--- srcs/Commands/ServerQueries/Motd.cpp
+++ srcs/Commands/ServerQueries/Motd.cpp
@@ -1,39 +1,52 @@
+//#include "../../includes/irc.hpp"
+//TODO: a deplacer au bon endroit
+#include <fstream>
 #include <IRC.hpp>
 /**
  * @brief send the content of motd file located in cmd_files. send first RPL_MOTDSTARD RPL_MOTD
  * then end with RPL_ENDOFMOTD. If no file located then ERR_NOMOTD
+ * @param client
+ * @param server
  */
 void Commands::motd_cmd(User *user, IRC *server)
 {
+    (void)user;
+    (void)server;
     #if DEBUG == 1
-        std::cout << RED << "ENTER MOTD CMD" << NC << std::endl;
+        std::cout << "DEBUG: enter motd_cmd" << std::endl;
     #endif
-    std::vector<std::string> params;
+    std::vector<std::string> reply_params;
+    std::string reply;
 
     std::ifstream infile("./cmd_files/motd");
     if (infile.is_open())
     {
-        params.push_back(user->get_hostname());
-        server->send_rpl("375", user, params, "");
-        params.clear();
-
         std::string buff;
         for (std::string line; getline(infile, line);)
         {
             line = "- " + line + "\r\n";
-            //buff.append(line);
-            params.push_back(buff);
-            params.push_back(line);
-            server->send_rpl("372", user, params, "");
-            params.clear();
+            buff.append(line);
         }
 
-        server->send_rpl("376", user, params, "");
-        params.clear();
+        reply_params.push_back(user->get_hostname());
+        reply = build_reply(375, user, reply_params);
+        server->_response_queue.push_back(std::make_pair(user->get_socket(), reply));
+        reply_params.clear();
+
+        reply_params.push_back(buff);
+        reply = build_reply(372, user, reply_params);
+        server->_response_queue.push_back(std::make_pair(user->get_socket(), reply));
+        reply_params.clear();
+
+
+        reply = build_reply(376, user, reply_params);
+        server->_response_queue.push_back(std::make_pair(user->get_socket(), reply));
+        reply_params.clear();
     }
     else
-        server->send_rpl("422", user, params, "");
-    #if DEBUG == 1
-        std::cout << RED << "EXIT MOTD CMD" << NC << std::endl;
-    #endif
+    {
+        reply = build_reply(422, user, reply_params);
+        server->_response_queue.push_back(std::make_pair(user->get_socket(), reply));
+        reply_params.clear();
+    }
 }
