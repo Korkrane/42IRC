d8a3d51ea7e6fec4c468dfd160803ef73e2a86e6
Korkrane <baudoin.haas@gmail.com>
2021-12-18 15:48:44 +0100

    ADD: invite cmd

--- srcs/Commands/ChannelOperations/Invite.cpp
+++ srcs/Commands/ChannelOperations/Invite.cpp
@@ -1,164 +1,75 @@
 #include <IRC.hpp>
 
-/**
- * @brief
- *
- * @param target_channel
- * @param user
- * @param server
- * @return Channel*
- */
-Channel *Commands::find_target_channel(std::string target_channel, User *user, IRC *server)
-{
-    (void)target_channel;
-    (void)user;
-    (void)server;
-
-    std::vector<Channel *>::iterator it;
-    std::vector<Channel *>::iterator ite;
-    std::vector<Channel *> channels = server->get_channels();
-    it = channels.begin();
-    ite = channels.end();
-    std::string check_target;
-    while (it != ite)
-    {
-        check_target = (*it)->get_name();
-        if (check_target.compare(target_channel) == 0)
-            return (*it);
-        it++;
-    }
-#if DEBUG == 1
-    std::cout << BLUE << "DEBUG: "
-              << "did not find target_nick" << std::endl;
-#endif
-    return (NULL);
-}
-
-/**
- * @brief
- *
- * @param target_nick
- * @param user
- * @param server
- * @return User*
- */
-User *Commands::find_target_nick(std::string target_nick, User *user, IRC *server)
-{
-    (void)target_nick;
-    (void)user;
-    (void)server;
-
-    std::vector<User *>::iterator it;
-    std::vector<User *>::iterator ite;
-    it = server->get_users().begin();
-    ite = server->get_users().end();
-    std::string check_nick;
-    while (it != ite)
-    {
-        check_nick = (*it)->get_nickname();
-        if (check_nick.compare(target_nick) == 0)
-            return (*it);
-        it++;
-    }
-#if DEBUG == 1
-    std::cout << BLUE << "DEBUG: "
-              << "did not find target_nick" << std::endl;
-#endif
-    return (NULL);
-}
-
-/**
- * @brief
- *
- * @param user
- * @param server
- * Params: <nickname> <channel>
- * TODO: faire des tests pour voir l'ordre de priorite des erreurs
- */
 void Commands::invite(User *user, IRC *server)
 {
-    (void)user;
-    (void)server;
+	std::vector<std::string> params;
+	std::vector<std::string> reply_params;
 
-    //0. Preparation en cas d'erreur
-    std::vector<std::string> error;
-
-    //1. Verifier le nombre d'argument
-    if (user->get_params_size() != 2)
-    {
-#if DEBUG == 1
-        std::cout << BLUE << "DEBUG: "
-                  << "Invite command incorrect params size" << std::endl;
-#endif
-        //TODO: build reply
-        /*
-        error.push_back(user->get_command_name());
-        error_handler("461", user, NULL, error);
-        */
-        return;
-    }
-    //2. Verifier si le nickname correspond bien a quelqu'un
-    std::string target_nick = user->get_params().front();
-    User *target_user = find_target_nick(target_nick, user, server);
-    if (!target_user)
-    {
-        //TODO: build reply
-        /*
-        error.push_back(user->get_nickname());
-        error_handler("401", user, NULL, error);
-        */
-        return;
-    }
-    //3. Verifier si le channel correspond bien a un channel
-    std::string target_channel = user->get_params().back();
-    Channel *chan = find_target_channel(target_channel, user, server);
-    if (chan)
-    {
-        //TODO: a tester
-        //4. Verifier que le user est lui meme member
-        if (chan->user_is_member(user) == false) //NOTONCHANNEL
-        {
-            //TODO: build reply
-            /*
-            error.push_back(chan->get_name());
-            error_handler("442", user, chan, error);
-            */
-            return;
-        }
-        //5. Verifier que la channel n'est pas full
-        if (chan->get_members_nb() >= USER_MAXCHAN)
-        {
-            return;
-        }
-        //6. Verifier que celui qui invite n'est pas deja sur le channel
-        if (chan->user_is_member(target_user) == false)
-        {
-            //TODO: build reply
-            /*
-            error.push_back(user->get_nickname());
-            error.push_back(chan->get_name());
-            error_handler("443", user, chan, error);
-            */
-            return;
-        }
-        //7. Verifier que le target nick_name est connecte
-        if (server->find_user(target_nick) == true)
-        {
-            //TODO: build reply
-            /*
-            error.push_back(user->get_nickname());
-            error_handler("401", user, chan, error);
-            */
-            return;
-        }
-        //8. Ajouter au channel l'user
-        //chan->addMember(target_nick)
-        User *target_user = server->get_user_ptr(target_nick);
-        chan->newMember(target_user, false);
-
-        //9. Envoyer le message au serveur
-        //TODO: a reprendre en changeant les params (build reply)
-        //server->send_rpl("431", user, chan, chan->get_name() + " " + target_user->get_nickname());
-    }
-    return;
+	params = user->get_params();
+	//1. Verifier le nombre d'argument
+	if (params.size() < 2)
+	{
+		reply_params.push_back(user->get_command_name());
+		server->send_rpl("461", user, reply_params, "");
+		return;
+	}
+	//2. Verifier si le nickname correspond bien a quelqu'un
+	User *target_user = server->get_user_ptr(params[0]);
+	if (!target_user)
+	{
+		reply_params.push_back(params[0]);
+		server->send_rpl("401", user, reply_params, "");
+		return;
+	}
+	//3. Verifier si le channel correspond bien a un channel
+	Channel *chan = server->get_channel_ptr(params[1]);
+	if (chan)
+	{
+		//4. Verifier que le user est lui meme member
+		if (chan->user_is_member(user) == false) //NOTONCHANNEL
+		{
+			reply_params.push_back(params[1]);
+			server->send_rpl("442", user, reply_params, "");
+		}
+		//5. Verifier que chan si invite-only, user has op privilege
+		else if (chan->get_modes().find('i') != std::string::npos && !chan->user_is_operator(user))
+		{
+			reply_params.push_back(params[1]);
+			server->send_rpl("482", user, reply_params, "");
+		}
+		//6. Verifier que celui qui invite n'est pas deja sur le channel
+		else if (chan->user_is_member(target_user) == true)
+		{
+			reply_params.push_back(params[0]);
+			reply_params.push_back(params[1]);
+			server->send_rpl("443", user, reply_params, "");
+		}
+		else
+		{
+			reply_params.push_back(target_user->get_nickname());
+			reply_params.push_back(chan->get_name());
+			server->send_rpl("341", user, reply_params, "");
+			reply_params.clear();
+			if (chan->user_is_operator(user))
+			{
+				std::string message = ":" + user->get_nickname() + " invited " + target_user->get_nickname() + " into the channel.";
+				reply_params.push_back(user->get_hostname());
+				reply_params.push_back("@" + chan->get_name());
+				reply_params.push_back(message);
+				server->send_rpl("server", user, reply_params, "NOTICE");
+				reply_params.clear();
+			}
+			reply_params.push_back(user->get_nickname());
+			reply_params.push_back(user->get_username());
+			reply_params.push_back(user->get_hostname());
+			reply_params.push_back(target_user->get_nickname());
+			reply_params.push_back(":" + chan->get_name());
+			server->send_rpl("1499", target_user, reply_params, "INVITE");
+		}
+	}
+	else
+	{
+		reply_params.push_back(params[1]);
+		server->send_rpl("403", user, reply_params, "");
+	}
 }
