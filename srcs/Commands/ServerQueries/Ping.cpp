75cfdace7ff8b47e7e9953e99a14b81433e1fe3f
bahaas <baudoin.haas@gmail.com>
2021-12-22 21:10:25 +0100

    RFCTO: servQueries, OptFeatures, SendMssg

--- srcs/Commands/ServerQueries/Ping.cpp
+++ srcs/Commands/ServerQueries/Ping.cpp
@@ -1,14 +1,14 @@
-#include <IRC.hpp>
+#include "IRC.hpp"
 
 void Commands::ping_cmd(User *user, IRC *server)
 {
-    #if DEBUG == 1
-        std::cout << BLUE << "ENTER PING CMD" << NC << std::endl;
-    #endif
-    std::vector<std::string> params;
+#if DEBUG == 1
+    std::cout << BLUE << "ENTER PING CMD" << NC << std::endl;
+#endif
+    std::vector<std::string> reply_params;
 
-    server->send_rpl("999", user, params, "");
-    #if DEBUG == 1
-        std::cout << RED << "EXIT PING CMD" << NC << std::endl;
-    #endif
+    server->send_rpl("999", user, reply_params, "");
+#if DEBUG == 1
+    std::cout << RED << "EXIT PING CMD" << NC << std::endl;
+#endif
 }
--- srcs/Commands/ServerQueries/Version.cpp
+++ srcs/Commands/ServerQueries/Version.cpp
@@ -1,21 +1,21 @@
-#include <IRC.hpp>
+#include "IRC.hpp"
 
 void Commands::version_cmd(User *user, IRC *server)
 {
-    std::vector<std::string> params;
+    std::vector<std::string> reply_params;
 
     if (user->get_params().empty() || (user->get_params()[0] == server->get_name()))
     {
         // TODO check for degublevel & comments (hardcoded atm)
-        params.push_back(server->get_version());
-        params.push_back("debuglevel00");
-        params.push_back(server->get_name());
-        params.push_back("random comments as test");
-        server->send_rpl("351", user, params, "");
+        reply_params.push_back(server->get_version());
+        reply_params.push_back("debuglevel00");
+        reply_params.push_back(server->get_name());
+        reply_params.push_back("random comments as test");
+        server->send_rpl("351", user, reply_params, "");
     }
     else if (!user->get_params().empty() && user->get_params()[0] != server->get_name())
     {
-        params.push_back(user->get_params()[0]);
-        server->send_rpl("402", user, params, "");
+        reply_params.push_back(user->get_params()[0]);
+        server->send_rpl("402", user, reply_params, "");
     }
 }
