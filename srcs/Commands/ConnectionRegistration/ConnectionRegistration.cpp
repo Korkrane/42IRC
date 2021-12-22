bcb48b7c77147d985862b02bf315aa014bf8b182
malatini <malatini@student.42.fr>
2021-12-22 10:11:02 +0100

    petite correc comment pass

--- srcs/Commands/ConnectionRegistration/ConnectionRegistration.cpp
+++ srcs/Commands/ConnectionRegistration/ConnectionRegistration.cpp
@@ -11,10 +11,10 @@ void Commands::welcome_cmd(User *user, IRC *server)
     server->send_rpl("001", user, params, "");
     params.clear();
 
-    //reply 2
-    params.push_back(user->get_hostname());
-    server->send_rpl("002", user, params, "");
-    params.clear();
+    //reply 2 - PATH A revoir
+   // params.push_back(user->get_hostname());
+    //server->send_rpl("002", user, params, "");
+    //params.clear();
 
     //reply 3
     params.push_back(server->get_server_creation());
